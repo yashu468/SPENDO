7a7ff42ef531515c4833fb7272bbe32f78d364cd
yashu468 <yashaswivem@gmail.com>
2025-11-10 15:23:57 +0530

    Update categories.js

--- src/constants/categories.js
+++ src/constants/categories.js
@@ -1,5 +1,5 @@
 const incomeColors = ['rgba(18, 49, 35, 1)', '#154731', '#168354ff', '#128a58ff', '#068d57ff', '#10ac6e', '#038d58ff', '#04e38d', '#00ff9d'];
-const expenseColors = ['#5a0b0eff', '#68140aff', '#94301cff', '#8a2b13ff', '#d84c34ff', '#ca481dff', '#ee8d68', '#f79d79', '#ffae8a', '#cc474b', '#f55b5f'];
+const expenseColors = ['#5a0b0eff', '#862115ff', '#bb3419ff', '#b63515ff', '#d84c34ff', '#ca481dff', '#ee8d68', '#f79d79', '#ffae8a', '#cc474b', '#f55b5f'];
 
 export const incomeCategories = [
   { type: 'Business', amount: 0, color: incomeColors[0] },
@@ -30,4 +30,5 @@ export const expenseCategories = [
 export const resetCategories = () => {
   incomeCategories.forEach((c) => c.amount = 0);
   expenseCategories.forEach((c) => c.amount = 0);
+
 };
